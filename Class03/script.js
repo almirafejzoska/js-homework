7f84490817f48f85d11396fae8e62bf017644bcb
almirafejzoska <almirafejzoska@protonmail.com>
2023-12-13 00:38:25 +0100

    korekcija

--- Class03/script.js
+++ Class03/script.js
@@ -40,14 +40,33 @@ console.log(notDefined);
 //     Calculates your dog's age based on the conversion rate of 1 human year to 7 dog years
 //     Note: Call the function in console to see answer
 //     Bonus: Make the same function work for converting dog to human years as well
-
-function yearsOfDogAndHuman(dogYearsAndHumanYears) {
-  let dogYears = dogYearsAndHumanYears * 7;
-  let humanYears = dogYearsAndHumanYears / 7;
-  return `A human's age in dog years is ${dogYears}; a dog's age in human years is ${humanYears}.`
+function dogToHuman(years) {
+  let convertedYear = years * 7;
+  return convertedYear;
 }
-const calculateAge = yearsOfDogAndHuman(24, 5);
-console.log(calculateAge);
+function humanToDog(years) {
+  let convertedYear = parseFloat(years / 7);
+  return convertedYear;
+}
+function mistakeMade() {
+  return "This option doesn't exist, please try again.";
+}
+
+let chooseOption = prompt("Would you like to convert Dog To Human (1) or Human To Dog (2) years:");
+
+if (chooseOption === "I choose option one" || chooseOption == 1 || chooseOption == "Dog To Human") {
+  let optionOne = prompt("Enter dog's age:");
+  alert("The dog is " + `${dogToHuman(optionOne)}` + " years old in human age.");
+}
+else if (chooseOption === "2") {
+  let optionTwo = prompt("Enter human's age:");
+  let formatedAge = humanToDog(optionTwo);
+  alert("The human is " + `${formatedAge.toPrecision(2)}` + " years old in dog age.");
+} 
+else {
+  console.log(mistakeMade());
+  alert(mistakeMade());
+}
 
 // HOMEWORK Part 3
 
@@ -59,17 +78,22 @@ console.log(calculateAge);
 //     Note: Hardcode your account money in the program
 //     Bonus: Make it work with prompt()!
 
+
 function atm(cash) {
   let currentBalanceInBank = 25000;
-  if (cash > currentBalanceInBank) {
-    return "Not enough money";
+  if (cash == 0) {
+    return "You cannot withdraw 0.";
+  }
+  else if (currentBalanceInBank >= cash) {
+    return `You withdrew ${cash} and you have ${currentBalanceInBank - cash} left on the account.`;
+  }
+  else if (cash > currentBalanceInBank) {
+    return "Not enough funds.";
   }
   else {
-    currentBalanceInBank -= cash;
-    return `You withdrew ${cash} and you have ${currentBalanceInBank} left on the account:`;
+    return "Please enter valid amount.";
   }
 }
-let withdrawCash = prompt("Please enter amount of cash to withdraw:")
-let parsedwithdrawCash = parseInt(withdrawCash);
-let withdraw = atm(parsedwithdrawCash);
-console.log(withdraw);
+
+let printATM = prompt("Please enter amount of cash to withdraw:");
+alert(atm(printATM));
